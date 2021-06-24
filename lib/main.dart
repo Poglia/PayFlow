540821cdedd28c0bf99167b96be0550e641522b4
Pedro Poglia <pedro.poglia@hotmail.com>
2021-06-23 23:27:29 -0300

    Creating and Styling Splash Screen and Login Screen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+// import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+// import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+// ignore: use_key_in_widget_constructors
+class AppWidget extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: LoginPage(),
+    );
+  }
+}
